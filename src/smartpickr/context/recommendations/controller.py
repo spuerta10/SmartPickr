e44910819b87cd39ea14866f4faa641b54be0c1a
spuerta <puertasantiago2002@gmail.com>
2025-08-15 17:08:10 -0500

    feat: Added API call to recommender pipeline

--- src/smartpickr/context/recommendations/controller.py
+++ src/smartpickr/context/recommendations/controller.py
@@ -1,48 +1,47 @@
-from time import sleep  # TODO: remove after implementing real recommendation logic
+import requests
 
 from shared.base_controller import BaseController
 from model.anime import Anime
-from context.manager import AppContext
+from context.manager import AppContext, AppContextManager
 
 class RecommendationsController(BaseController):
     __instance = None
     
-    def __new__(cls, animes: list[Anime] = None):
+    def __new__(cls, recommender_url: str = None, animes: list[Anime] = None):
         if cls.__instance is None:
             cls.__instance: "RecommendationsController" = super().__new__(cls)
             cls.__instance._initialized = False
         return cls.__instance
     
-    def __init__(self, animes: list[Anime] = None):
+    def __init__(self, recommender_url: str = None, animes: list[Anime] = None):
         if not self._initialized:
             animes = animes or []
+            self.recommender_url: str = recommender_url
             super().__init__(type=AppContext.RECOMMENDATIONS, animes=animes)
             self._initialized = True
         
-    def get_recommendations(self, n_recommendations: int = 1) -> list[Anime]:
-        sleep(3)  # simulate a delay for fetching recommendations
-        mocked_recommendations: list[Anime] = [
-            Anime(
-                id=4,
-                title="Fullmetal Alchemist: Brotherhood",
-                description="A saiyan’s journey.",
-                image_url="https://imgsrv.crunchyroll.com/cdn-cgi/image/fit=contain,format=auto,quality=70,width=320,height=180/catalog/crunchyroll/025260e7ab620e093bff1233dd78629d.jpg",
-                seasons=1,
-                episodes=64
-            ),
-            Anime(
-                id=5,
-                title="Neon Genesis Evangelion",
-                description="A saiyan’s journey.",
-                image_url="https://wiki.evageeks.org/images/thumb/c/ca/26_C343_shinji-grin.jpg/260px-26_C343_shinji-grin.jpg",
-                seasons=1,
-                episodes=26
-            ),
-        ]
-        
-        return mocked_recommendations 
+    def get_recommendations(self, n_recommendations: int = 1) -> list[Anime]:     
+        assert self.recommender_url
+        try:
+            response = requests.post(
+                self.recommender_url,
+                json={  # TODO: For mantainibility it'd be better to define this in another object
+                    "ratings": self.get_user_ratings(), 
+                    "n_recommendations": n_recommendations
+                },
+                timeout=600
+            )
+            response.raise_for_status()
+            data: dict = response.json()
+            recommendations: list[Anime] = [Anime(**recommendation) for recommendation in data.get("recommendations")]
+            return recommendations
+        except Exception as e:
+            raise e
         
     def add_recommendations(self, recommendations: list[Anime]) -> None:
         self.animes.extend(recommendations)
+
+    def get_recommendations_count(self, liked: bool = True) -> int:
+        return sum(1 for rating in self.get_user_ratings() if rating["liked"] == liked)
         
         
