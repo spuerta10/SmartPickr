d5c5229927f3066529ab5160cec367a32ff6dfb6
spuerta <puertasantiago2002@gmail.com>
2025-08-15 17:07:52 -0500

    feat: Added summarization view handler

--- src/smartpickr/main.py
+++ src/smartpickr/main.py
@@ -2,8 +2,7 @@ import streamlit as st
 
 from context.manager import AppContextManager, AppContext
 from context.rating.handler import handle_init_rating
-#from context.recommendations.views.loading import LoadingView
-from context.recommendations.handler import handle_load, handle_recommendations
+from context.recommendations.handler import handle_load, handle_recommendations, handle_summary
 
 if __name__ == "__main__":
     if AppContextManager.get_current_context() == AppContext.INITIAL_RATING:
@@ -16,5 +15,4 @@ if __name__ == "__main__":
         handle_recommendations()
     
     elif AppContextManager.get_current_context() == AppContext.COMPLETE:
-        st.session_state.clear()  # wipe all session variables
-        st.rerun()  # restart the script from top
+        handle_summary()
